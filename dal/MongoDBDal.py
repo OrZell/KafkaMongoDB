776dec11bd9d7887420709125f2868b38c918d76
Or Zellinger <or.zellinger@gmail.com>
2025-08-26 18:21:05 +0300

    add fetch method

--- dal/MongoDBDal.py
+++ dal/MongoDBDal.py
@@ -11,12 +11,24 @@ class MongoDBDal:
     def open_connection(self):
         if self.connection is None:
             self.connection = MongoClient(self.connecting_string)
+        return self.connection
 
     def close_connection(self):
         if self.connection:
             self.connection.close()
 
     def insert_documents(self, topic, documents):
-        self.open_connection()
-        self.connection[self.db][topic].insert_many(documents)
-        self.close_connection()
+        if documents:
+            self.open_connection()
+            self.connection[self.db][topic].insert_many(documents)
+            self.close_connection()
+
+    def find_not_interesting(self):
+        connection = self.open_connection()
+        events = connection[self.db]['not_interesting'].find()
+        return events
+
+    def find_interesting(self):
+        connection = self.open_connection()
+        events = connection[self.db]['interesting'].find()
+        return events
